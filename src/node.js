aee07a622a26d9850c8aeb90e95b8c7bc64728d6
anechka396 <anechka3960@gmail.com>
2019-10-15 15:00:52 +0300

    feat: add node and queue realisation

--- src/node.js
+++ src/node.js
@@ -1,22 +1,69 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+		if(this.left && this.right) return;
+		if(!this.left) {
+			this.left = node;
+			node.parent = this;
+		} else {
+			this.right = node;
+			node.parent = this;
+		}
 	}
 
 	removeChild(node) {
-
+		if(node === this.left) {
+			this.left = null;
+			node.parent = null;
+		} else if(node === this.right) {
+			this.right = null;
+			node.parent = null;
+		} else {
+			throw Error;
+		}
 	}
 
 	remove() {
-
+		if(this.parent) {
+			this.parent.removeChild(this);
+		}
 	}
 
 	swapWithParent() {
-		
+		if(this.parent) {
+			var left = this.left;
+			var right = this.right;
+			var curParent = this.parent;
+			var curGrandParent = this.parent.parent;
+			
+			if(curGrandParent)
+				curGrandParent.left === curParent ? (curGrandParent.left = this) : (curGrandParent.right = this);
+			this.parent = curGrandParent;
+			
+			if(this === curParent.left) {
+				this.left = curParent;
+				this.right = curParent.right;
+				if (this.right) this.right.parent = this;
+			} else {
+				this.right = curParent;
+				this.left = curParent.left;
+				if (this.left) this.left.parent = this;
+			}
+			
+			curParent.left = left;
+			curParent.right = right;
+			curParent.parent = this;
+			
+			if(left) left.parent = curParent;
+			if(right) right.parent = curParent;
+		}
 	}
 }
 
--- src/queue.js
+++ src/queue.js
@@ -1,24 +1,27 @@
 const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
-	constructor(maxSize) {
-
+	constructor(maxSize = 30) {
+		this.maxSize = maxSize;
+		this.heap = new MaxHeap;
 	}
 
 	push(data, priority) {
-
+		if(this.size() === this.maxSize) throw Error;
+		this.heap.push(data, priority);
 	}
 
 	shift() {
-
+		if(this.size() === 0) throw Error;
+		this.heap.pop();
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		return this.heap.isEmpty();
 	}
 }
 
